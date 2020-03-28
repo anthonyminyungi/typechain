92a8fcd25bdb70bc6f41c8a87cc99e6d61f2a56a
anthonyminyungi <dbstnsdl12@naver.com>
2020-03-28 16:17:49 +0900

    Practice for interface and class usage in ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `hello ${name}, you are ${age}, and you are a ${gender}!!`;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const lynn = new Human('lynn', 18, 'female');
+
+const sayHi = (person: Human): string => {
+  return `hello ${person.name}, you are ${person.age}, and you are a ${person.gender}!!`;
 };
 
-console.log(sayHi('Nicolas', 24, 'male'));
+console.log(sayHi(lynn));
 
 export {};
